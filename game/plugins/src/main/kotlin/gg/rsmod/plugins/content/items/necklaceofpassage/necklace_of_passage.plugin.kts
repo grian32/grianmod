9e2b2e1728d517a47c33344ec1416445a431919e
Grian <grianguy32@gmail.com>
2021-10-22 18:02:23 +0300

    more or less finalize necklace of passage

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/necklaceofpassage/necklace_of_passage.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/necklaceofpassage/necklace_of_passage.plugin.kts
@@ -1,13 +1,10 @@
+/**
+ * @author Grian
+ */
 package gg.rsmod.plugins.content.items.necklaceofpassage
 
 import gg.rsmod.plugins.content.magic.TeleportType
 import gg.rsmod.plugins.content.magic.teleport
-
-load_metadata {
-    author = "Grian"
-    description = "Implements the Necklace of Passage"
-    name = "Necklace of Passage"
-}
 
 val NECKLACES = listOf(
     Items.NECKLACE_OF_PASSAGE1,
@@ -23,6 +20,7 @@ private val LOCATIONS = mapOf(
     "Eagles' Eyrie" to Tile(3405, 3158)
 )
 
+// TODO: chat messages for degrading in general
 NECKLACES.forEach { necklace ->
     LOCATIONS.forEach { (name, tile) ->
         on_equipment_option(necklace, name) {
@@ -49,11 +47,18 @@ NECKLACES.forEach { necklace ->
                 title = "<col=800000>Teleport to...</col>"
             )
 
+            val slot = player.getInteractingItemSlot();
+
+            when (player.inventory[slot]?.id) {
+                Items.NECKLACE_OF_PASSAGE5 -> player.inventory[slot] = Item(Items.NECKLACE_OF_PASSAGE4, 1)
+                Items.NECKLACE_OF_PASSAGE4 -> player.inventory[slot] = Item(Items.NECKLACE_OF_PASSAGE3, 1)
+                Items.NECKLACE_OF_PASSAGE3 -> player.inventory[slot] = Item(Items.NECKLACE_OF_PASSAGE2, 1)
+                Items.NECKLACE_OF_PASSAGE2 -> player.inventory[slot] = Item(Items.NECKLACE_OF_PASSAGE1, 1)
+                Items.NECKLACE_OF_PASSAGE1 -> player.inventory[slot] = null
+                else -> println("Something went really wrong with Necklaces Of Passage! D:")
+            }
+
             when (option) {
-                /**
-                 * TODO: figure out a way to see which item in inventory got rubbed, and degrade it from there
-                 * Currently doesn't degrade because of this.
-                 */
                 // TODO: abstract this into a function once code is finalized
                 // was getting @ me over nulls so i just hardcoded the tile
                 1 -> {
