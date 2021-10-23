0af83c74d0c7c124fc65e5001c2044cc74b42832
Grian <grianguy32@gmail.com>
2021-10-23 23:59:32 +0300

    finalize outpost, rsps ladders

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/outpost/outpost.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/outpost/outpost.plugin.kts
@@ -1,22 +1,17 @@
 package gg.rsmod.plugins.content.areas.outpost
 
-import gg.rsmod.plugins.content.magic.TeleportType
-import gg.rsmod.plugins.content.magic.teleport
-
-// TODO: face right direction
-
-on_obj_option(16683, "climb-up") {
+on_obj_option(Objs.LADDER_16683, "climb-up") {
     if (player.tile == Tile(2435, 3346)) {
-        player.teleport(Tile(2435, 3346, 1), TeleportType.CLIMB_UP_LADDER)
+        player.moveTo(Tile(2435, 3346, 1))
     } else if (player.tile == Tile(2436, 3346, 1)) {
-        player.teleport(Tile(2436, 3346, 2), TeleportType.CLIMB_UP_LADDER)
+        player.moveTo(Tile(2436, 3346, 2))
     }
 }
 
-on_obj_option(16679, "climb-down") {
+on_obj_option(Objs.LADDER_16679, "climb-down") {
     if (player.tile == Tile(2435, 3346, 1)) {
-        player.teleport(Tile(2435, 3346), TeleportType.CLIMB_DOWN_LADDER)
+        player.moveTo(Tile(2435, 3346))
     } else if (player.tile == Tile(2436, 3346, 2)) {
-        player.teleport(Tile(2436, 3346 , 1), TeleportType.CLIMB_DOWN_LADDER)
+        player.moveTo(Tile(2436, 3346 , 1))
     }
 }
