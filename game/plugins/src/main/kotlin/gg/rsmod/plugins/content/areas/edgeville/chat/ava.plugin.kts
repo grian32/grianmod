d382e155d0aea973884c17c10403c50a2fc4b982
Grian <grianguy32@gmail.com>
2021-11-05 17:48:35 +0200

    best thing ive ever made

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/edgeville/chat/ava.plugin.kts
@@ -0,0 +1,25 @@
+package gg.rsmod.plugins.content.areas.edgeville.chat
+
+on_npc_option(Npcs.AVA, "talk-to") {
+    player.queue {
+        chatNpc("Would you like to buy an Ava's Accumulator for 25,000 coins?", Npcs.AVA, title = "Ava")
+        val option = options(
+            "Yes, please!",
+            "No! Fuck off!"
+        )
+
+        when (option) {
+            1 -> {
+                if (player.inventory.remove(Items.COINS_995, 25_000, true).hasSucceeded()) {
+                    player.inventory.add(Items.AVAS_ACCUMULATOR)
+                } else {
+                    player.message("You don't have enough coins!")
+                }
+            }
+            2 -> {
+                player.message("Ava slaps you.")
+                player.hit(1)
+            }
+        }
+    }
+}
