3de93013368c7570229aee7845a31914263668ba
Grian <grianguy32@gmail.com>
2021-11-18 10:06:56 +0200

    fix harralander potion

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/herblore/unf_pots.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/herblore/unf_pots.plugin.kts
@@ -7,7 +7,7 @@ private val unfPots = listOf(
     UnfPot(Items.GUAM_LEAF, Items.GUAM_POTION_UNF, 3, "Guam Leaf"),
     UnfPot(Items.MARRENTILL, Items.MARRENTILL_POTION_UNF, 5, "Marrentill"),
     UnfPot(Items.TARROMIN, Items.TARROMIN_POTION_UNF, 12, "Tarromin"),
-    UnfPot(Items.HARRALANDER_POTION_UNF, Items.HARRALANDER_POTION_UNF, 22, "Harralander"),
+    UnfPot(Items.HARRALANDER, Items.HARRALANDER_POTION_UNF, 22, "Harralander"),
     UnfPot(Items.RANARR_WEED, Items.RANARR_POTION_UNF, 30, "Ranarr Weed"),
     UnfPot(Items.TOADFLAX, Items.TOADFLAX_POTION_UNF, 34, "Toadflax"),
     UnfPot(Items.IRIT_LEAF, Items.IRIT_POTION_UNF, 45, "Irit Leaf"),
