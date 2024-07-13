2c76707cdab6d133ed6ed65ee41eb25330a4ddcf
grian <grianguy32@gmail.com>
2024-07-13 19:23:42 +0300

    fixes resupply amount in zeke shop

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/alkharid/shops/zeke_superior_scimitars.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/alkharid/shops/zeke_superior_scimitars.plugin.kts
@@ -3,8 +3,8 @@ package gg.rsmod.plugins.content.areas.alkharid.shops
 import gg.rsmod.plugins.content.mechanics.shops.CoinCurrency
 
 createShop("Zeke's Superior Scimitar", CoinCurrency()) {
-    items[0] = ShopItem(Items.BRONZE_SCIMITAR, 5, 32, 17, 5, 100)
-    items[1] = ShopItem(Items.IRON_SCIMITAR, 3, 112, 61, 3, 200)
-    items[2] = ShopItem(Items.STEEL_SCIMITAR, 2, 400, 220, 2, 600)
+    items[0] = ShopItem(Items.BRONZE_SCIMITAR, 5, 32, 17, 1, 100)
+    items[1] = ShopItem(Items.IRON_SCIMITAR, 3, 112, 61, 1, 200)
+    items[2] = ShopItem(Items.STEEL_SCIMITAR, 2, 400, 220, 1, 600)
     items[3] = ShopItem(Items.MITHRIL_SCIMITAR, 1, 1040, 572, 1, 4000)
 }
