3c478782d975a2915ec75cbed3daceb4aac376ef
grian <grianguy32@gmail.com>
2024-07-13 19:33:26 +0300

    dommik shop

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/alkharid/chat/dommik.plugin.kts
@@ -0,0 +1,26 @@
+package gg.rsmod.plugins.content.areas.alkharid.chat
+
+onNpcOption(Npcs.DOMMIK, "Trade") {
+    player.openShop("Dommik's Crafting Store.")
+}
+
+onNpcOption(Npcs.DOMMIK, "Talk-to") {
+    player.queue {
+        chatNpc("Would you like to buy some crafting equipment?")
+        val option = options(
+            "No thanks; I've got all the Crafting equipment I need.",
+            "Let's see what you've got then."
+        )
+
+        when (option) {
+            1 -> {
+                chatPlayer("No thanks; I've got all the Crafting equipment I need.")
+                chatNpc("Okay. Fare well on your travels.")
+            }
+
+            2 -> {
+                player.openShop("Dommik's Crafting Store.")
+            }
+        }
+    }
+}
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/alkharid/spawns/dommik.plugin.kts
@@ -0,0 +1,3 @@
+package gg.rsmod.plugins.content.areas.alkharid.spawns
+
+spawnNpc(npc = Npcs.DOMMIK, x = 3321, z = 3194, walkRadius = 3)
